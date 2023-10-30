7dd5bde56337621eef7539f4ae04d33da74dd97d
Cuvii <xw5427@gmail.com>
2023-10-31 00:02:20 +0800

    chore: init

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+const isClient = typeof window !== "undefined";
+
+const allDevtools = {
+  nuxt: () => {
+    // @ts-ignore I'm nuxt
+    isClient && (window.$nuxt = "🤡");
+  },
+  vue: () => {
+    // @ts-ignore I'm nuxt
+    isClient && (window.__VUE__ = "🤡");
+  },
+  svelte: () => {
+    isClient &&
+      // @ts-ignore I'm nuxt
+      (window.__svelte = {
+        v: new Set("🤡"),
+      });
+  },
+  angular: () => {
+    isClient && document.body.setAttribute("ng-version", "🤡");
+  },
+  redux: () => {
+    isClient &&
+      // @ts-ignore I'm nuxt
+      (window.__REDUX_DEVTOOLS_EXTENSION__ = {
+        connect: () => "🤡",
+      });
+  },
+  motion: () => {
+    // @ts-ignore I'm motion
+    window.__MOTION_DEV_TOOLS = "🤡";
+  },
+};
+
+type Devtools = keyof typeof allDevtools;
+
+export default function devtuls(devtools: Devtools) {
+  allDevtools[devtools]?.();
+}
