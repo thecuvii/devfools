ee35533fac03e8b1ed89a95ecab02c9e7fcbdb4b
withyellow <xw5427@gmail.com>
2023-11-02 17:28:20 +0800

    chore: update example to vanilla js

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 import path from "node:path";
+import UnoCSS from "unocss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [UnoCSS()],
   resolve: {
     alias: {
       "@/": path.join(__dirname, "../src/index.ts"),
