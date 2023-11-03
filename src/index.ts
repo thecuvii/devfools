35bdcf4336944fa1046d592af2138eb0ac370371
Cuvii <xw5427@gmail.com>
2023-11-03 14:37:34 +0800

    refactor: update structure

--- src/index.ts
+++ src/index.ts
@@ -15,192 +15,267 @@ const defineWindowProperty = (name: string, value: any) => {
   })
 }
 
+type Config = {
+  packageName?: string
+  enable: () => void
+  // disbale:()=>void TODO
+}
+
 const allDevtools = {
-  next: () => {
-    allDevtools.react()
-    defineWindowProperty('next', {
-      version: VersionMap.next,
-    })
+  next: {
+    enable: () => {
+      allDevtools.react.enable()
+      defineWindowProperty('next', {
+        version: VersionMap.next,
+      })
+    },
   },
-  vite: () => {
-    defineWindowProperty('__vite_is_modern_browser', true)
+  vite: {
+    enable: () => {
+      defineWindowProperty('__vite_is_modern_browser', true)
+    },
   },
-  vitepress: () => {
-    defineWindowProperty('__VP_HASH_MAP__', true)
-    defineWindowProperty('__vitepress', true)
+  vitepress: {
+    enable: () => {
+      defineWindowProperty('__VP_HASH_MAP__', true)
+      defineWindowProperty('__vitepress', true)
+    },
   },
-  element: () => {
-    const $div = document.createElement('div')
-    $div.classList.add('el-table-column')
-    document.body.appendChild($div)
+  element: {
+    enable: () => {
+      const $div = document.createElement('div')
+      $div.classList.add('el-table-column')
+      document.body.appendChild($div)
+    },
   },
+  solid: {
+    enable: () => {
+      defineWindowProperty('__SOLID_DEVTOOLS__', true)
+      defineWindowProperty('Solid$$', true)
+    },
+  },
+  react: {
+    enable: () => {
+      defineWindowProperty('__REACT_DEVTOOLS_ATTACH__', {})
 
-  solid: () => {
-    defineWindowProperty('__SOLID_DEVTOOLS__', true)
-    defineWindowProperty('Solid$$', true)
+      if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+        try {
+          window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject({
+            reconcilerVersion: '100.666',
+          })
+        } catch {}
+      }
+    },
   },
-  react: () => {
-    defineWindowProperty('__REACT_DEVTOOLS_ATTACH__', {})
-
-    if (window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
-      try {
-        window.__REACT_DEVTOOLS_GLOBAL_HOOK__.inject({
-          reconcilerVersion: '100.666',
-        })
-      } catch {}
-    }
-  },
-  nuxt: () => {
-    const $nuxt = {
-      config: {},
-      data: {},
-      path: '/',
-      state: {},
-      serverRendered: true,
-      $root: {
-        constructor: {
-          config: {
-            devtools: false,
+  nuxt: {
+    enable: () => {
+      const $nuxt = {
+        config: {},
+        data: {},
+        path: '/',
+        state: {},
+        serverRendered: true,
+        $root: {
+          constructor: {
+            config: {
+              devtools: false,
+            },
           },
         },
-      },
-    }
-    defineWindowProperty('$nuxt', $nuxt)
+      }
+      defineWindowProperty('$nuxt', $nuxt)
+    },
   },
-  vue: () => {
-    defineWindowProperty('__VUE__', true)
+  vue: {
+    enable: () => {
+      defineWindowProperty('__VUE__', true)
+    },
   },
-  svelte: () => {
-    defineWindowProperty('__svelte', {
-      v: new Set('🤡'),
-    })
+  svelte: {
+    enable: () => {
+      defineWindowProperty('__svelte', {
+        v: new Set('🤡'),
+      })
+    },
   },
-  angular: () => {
-    document.body.setAttribute('ng-version', '🤡')
+  angular: {
+    enable: () => {
+      document.body.setAttribute('ng-version', '🤡')
+    },
   },
-  redux: () => {
-    defineWindowProperty('__REDUX_DEVTOOLS_EXTENSION__', {
-      connect: () => '🤡',
-    })
+  redux: {
+    enable: () => {
+      defineWindowProperty('__REDUX_DEVTOOLS_EXTENSION__', {
+        connect: () => '🤡',
+      })
+    },
   },
-  motion: () => {
-    defineWindowProperty('__MOTION_DEV_TOOLS__', '🤡')
+  motion: {
+    enable: () => {
+      defineWindowProperty('__MOTION_DEV_TOOLS__', '🤡')
+    },
   },
-  framer: () => {
-    defineWindowProperty('Framer', {
-      version: '100',
-      Animatable: true,
-    })
-    defineWindowProperty('__framer_importFromPackage', true)
+  framer: {
+    enable: () => {
+      defineWindowProperty('Framer', {
+        version: '100',
+        Animatable: true,
+      })
+      defineWindowProperty('__framer_importFromPackage', true)
+    },
   },
-  gsap: () => {
-    defineWindowProperty('gsap', {
-      version: VersionMap.gsap,
-    })
+  gsap: {
+    enable: () => {
+      defineWindowProperty('gsap', {
+        version: VersionMap.gsap,
+      })
+    },
   },
-  three: () => {
-    defineWindowProperty('THREE', {
-      REVISION: VersionMap.THREE,
-    })
+  three: {
+    enable: () => {
+      defineWindowProperty('THREE', {
+        REVISION: VersionMap.THREE,
+      })
+    },
   },
-  mobx: () => {
-    defineWindowProperty('__mobxGlobal', '1')
-    defineWindowProperty('__mobxInstanceCount', '1')
-    defineWindowProperty('__mobxGlobals', '1')
+  mobx: {
+    enable: () => {
+      defineWindowProperty('__mobxGlobal', '1')
+      defineWindowProperty('__mobxInstanceCount', '1')
+      defineWindowProperty('__mobxGlobals', '1')
+    },
   },
-  tailwind: () => {
-    defineWindowProperty('tailwind', '🤡')
+  tailwind: {
+    enable: () => {
+      defineWindowProperty('tailwind', '🤡')
+    },
   },
-  rive: () => {
-    defineWindowProperty('rive', {
-      Rive: '',
-    })
+  rive: {
+    enable: () => {
+      defineWindowProperty('rive', {
+        Rive: '',
+      })
+    },
   },
-  antd: () => {
-    defineWindowProperty('antd', {
-      version: VersionMap.antd,
-    })
+  antd: {
+    enable: () => {
+      defineWindowProperty('antd', {
+        version: VersionMap.antd,
+      })
+    },
   },
-  naive: () => {
-    const $style = document.createElement('style')
-    $style.setAttribute('cssr-id', 'n-skeleton')
-    $style.innerHTML = `.n-skeleton {}`
-    document.head.appendChild($style)
+  naive: {
+    enable: () => {
+      const $style = document.createElement('style')
+      $style.setAttribute('cssr-id', 'n-skeleton')
+      $style.innerHTML = `.n-skeleton {}`
+      document.head.appendChild($style)
+    },
   },
-  manoco: () => {
-    defineWindowProperty('MonacoEnvironment', {})
-    defineWindowProperty('manoco', { editor: {} })
+  manoco: {
+    enable: () => {
+      defineWindowProperty('MonacoEnvironment', {})
+      defineWindowProperty('manoco', { editor: {} })
+    },
   },
-  codemirror: () => {
-    defineWindowProperty('CodeMirror', {
-      version: VersionMap.CodeMirror,
-    })
+  codemirror: {
+    enable: () => {
+      defineWindowProperty('CodeMirror', {
+        version: VersionMap.CodeMirror,
+      })
+    },
   },
-  vuepress: () => {
-    defineWindowProperty('__VUEPRESS__', {
-      version: VersionMap.vuepress,
-    })
+  vuepress: {
+    enable: () => {
+      defineWindowProperty('__VUEPRESS__', {
+        version: VersionMap.vuepress,
+      })
+    },
   },
-  remix: () => {
-    defineWindowProperty('__remixContext', {})
+  remix: {
+    enable: () => {
+      defineWindowProperty('__remixContext', {})
+    },
   },
-  nextui: () => {
-    const $style = document.createElement('style')
-    $style.innerHTML = `#fake-devfools {--nextui-colors-text: #000}`
-    document.head.appendChild($style)
+  nextui: {
+    enable: () => {
+      const $style = document.createElement('style')
+      $style.innerHTML = `#fake-devfools {--nextui-colors-text: #000}`
+      document.head.appendChild($style)
+    },
   },
-  material: () => {
-    defineWindowProperty('ngMaterial', {})
+  material: {
+    enable: () => {
+      defineWindowProperty('ngMaterial', {})
+    },
   },
-  pwa: () => {
-    const $link = document.createElement('link')
-    $link.setAttribute('rel', 'manifest')
-    document.head.appendChild($link)
+  pwa: {
+    enable: () => {
+      const $link = document.createElement('link')
+      $link.setAttribute('rel', 'manifest')
+      document.head.appendChild($link)
+    },
   },
-  sentry: () => {
-    defineWindowProperty('__SENTRY__', true)
-    defineWindowProperty('Raven', {
-      config: {},
-    })
-    defineWindowProperty('Sentry', {
-      SDK_VERSION: VersionMap.sentry,
-    })
+  sentry: {
+    enable: () => {
+      defineWindowProperty('__SENTRY__', true)
+      defineWindowProperty('Raven', {
+        config: {},
+      })
+      defineWindowProperty('Sentry', {
+        SDK_VERSION: VersionMap.sentry,
+      })
+    },
   },
-  styledComponent: () => {
-    const $div = document.createElement('div')
-    $div.setAttribute('sc-component-id', '🤡')
-    document.body.appendChild($div)
+  styledComponent: {
+    enable: () => {
+      const $div = document.createElement('div')
+      $div.setAttribute('sc-component-id', '🤡')
+      document.body.appendChild($div)
 
-    const $style = document.createElement('style')
-    $style.setAttribute('data-styled-version', '🤡')
-    document.head.appendChild($style)
+      const $style = document.createElement('style')
+      $style.setAttribute('data-styled-version', '🤡')
+      document.head.appendChild($style)
+    },
   },
-  webpack: () => {
-    defineWindowProperty('webpackChunk', {})
+  webpack: {
+    enable: () => {
+      defineWindowProperty('webpackChunk', {})
+    },
   },
-  googleAnalytics: () => {
-    defineWindowProperty('gaGlobal', {})
-    defineWindowProperty('GoogleAnalyticsObject', {})
+  googleAnalytics: {
+    enable: () => {
+      defineWindowProperty('gaGlobal', {})
+      defineWindowProperty('GoogleAnalyticsObject', {})
+    },
   },
-  umami: () => {
-    defineWindowProperty('umami', {})
+  umami: {
+    enable: () => {
+      defineWindowProperty('umami', {})
+    },
   },
-  babel: () => {
-    defineWindowProperty('_babelPolyfill', {})
+  babel: {
+    enable: () => {
+      defineWindowProperty('_babelPolyfill', {})
+    },
   },
-  hljs: () => {
-    defineWindowProperty('hljs', {
-      highlightBlock: {},
-      listLanguages: ['javascript'],
-    })
+  hljs: {
+    enable: () => {
+      defineWindowProperty('hljs', {
+        highlightBlock: {},
+        listLanguages: ['javascript'],
+      })
+    },
   },
-  prism: () => {
-    defineWindowProperty('Prism', {})
-    defineWindowProperty('apex', {
-      libVersions: { prismJs: { version: VersionMap.prism } },
-    })
+  prism: {
+    enable: () => {
+      defineWindowProperty('Prism', {})
+      defineWindowProperty('apex', {
+        libVersions: { prismJs: { version: VersionMap.prism } },
+      })
+    },
   },
-}
+} satisfies { [key: string]: Config }
 
 export type Devtools = keyof typeof allDevtools | 'all'
 export function devfools(devtools: 'all'): void
@@ -209,12 +284,12 @@ export function devfools(...devtools: Devtools[]) {
   if (!isClient) return
   if (devtools.length === 1 && devtools[0] === 'all')
     return Object.keys(allDevtools).forEach((devtools) =>
-      allDevtools[devtools as Exclude<Devtools, 'all'>](),
+      allDevtools[devtools as Exclude<Devtools, 'all'>].enable(),
     )
 
   devtools.forEach((devtool) => {
     if (devtool === 'all') return
-    allDevtools[devtool]()
+    allDevtools[devtool].enable()
   })
 }
 
